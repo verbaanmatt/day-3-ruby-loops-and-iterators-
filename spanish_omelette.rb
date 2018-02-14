0d0a9c1f2266d48e21b9bd01d9431e3c5956a34d
verbaanmatt <verbaanmatt@gmail.com>
2018-02-14 16:43:14 +0100

    Define the ingredients

--- spanish_omelette.rb
+++ spanish_omelette.rb
@@ -7,3 +7,15 @@ def print_progress_bar
   3.times { sleep 0.5; print "." }
   puts "\n"
 end
+
+
+NUM_POTATOES = 5
+NUM_EGGS = 6
+
+ingredients = [
+  { name: 'potatoes', quantity: NUM_POTATOES },
+  { name: 'eggs', quantity: NUM_EGGS },
+  { name: 'onion', quantity: 1 },
+  { name: 'oil', quantity: 'some' },
+  { name: 'salt', quantity: 'some' },
+]
